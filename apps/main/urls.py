0e3c78bced047327a579c86d50aebe39c553a931
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-03-12 20:38:59 +0500

    Added name= to login, logout, register urls.

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -5,9 +5,9 @@ from django.contrib.auth.views import LogoutView
 
 
 urlpatterns = [
-    path("login/", CustomLoginView.as_view()),
-    path("logout/", LogoutView.as_view()),
-    path("register/", RegisterPage.as_view()),
+    path("login/", CustomLoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("register/", RegisterPage.as_view(), name="register"),
 
     path('', Create, name="index"),
     path('all/', ListView, name="postlist"),
