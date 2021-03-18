7ee5ad6d124dc655b1494fce94034d7b53dcd9fc
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-03-18 19:57:30 +0500

    Added view classes GroupList, PublicGroupList and GroupDetail.

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,5 +1,3 @@
-import time
-from django.utils.text import slugify
 from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
@@ -11,7 +9,7 @@ from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth import authenticate, login, logout
 
-from .models import Article
+from .models import Article, GroupArticles
 from .forms import ArticleEditForm, UserCreationForm
 from .services import generate_url
 
@@ -28,17 +26,41 @@ class ArticleDetail(LoginRequiredMixin, DetailView):
 
 
 class ArticleCreate(LoginRequiredMixin, CreateView):
-	template_name = 'main/create.html'
-	form_class = ArticleEditForm
+    template_name = 'main/create.html'
+    form_class = ArticleEditForm
 
-	def form_valid(self, form):
-		Article.objects.update_or_create(
-			title=self.request.POST.get("title"),
-			body=self.request.POST.get("body"),
-			author=self.request.user,
-			url=slugify(self.request.POST.get("title") + "-" + str(time.strftime("%m-%d")))
-			)
-		return HttpResponseRedirect(slugify(self.request.POST.get("title") + "-" + str(time.strftime("%m-%d"))))
+    def form_valid(self, form):
+        Article.objects.update_or_create(
+            title=self.request.POST.get("title"),
+            body=self.request.POST.get("body"),
+            author=self.request.user,
+            url=generate_url(self.request))
+
+        return HttpResponseRedirect("post/"+generate_url(self.request))
+
+
+
+
+# GroupArticles views
+class PublicGroupList(LoginRequiredMixin, ListView):
+    model = GroupArticles
+    queryset = GroupArticles.objects.filter(private=False).order_by('-created_date')
+    context_object_name = 'group_list'
+    template_name = "main/p_group_list.html"
+
+
+class GroupList(LoginRequiredMixin, ListView):
+    model = GroupArticles
+    context_object_name = 'group_list'
+
+
+class GroupDetail(LoginRequiredMixin, DetailView):
+    model = GroupArticles
+    context_object_name = 'group'
+    slug_url_kwarg = 'slug'
+    query_pk_and_slug = True
+# end
+
 
 
 class CustomLoginView(LoginView):
@@ -51,7 +73,7 @@ class CustomLoginView(LoginView):
 
 
 class RegisterPage(FormView):
-    template_name = 'main/register.html'
+    template_name = 'main/registration.html'
     form_class = UserCreationForm
     redirect_authenticated_user = True
     success_url = reverse_lazy('index')
