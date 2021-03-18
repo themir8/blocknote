7cf5c1e5cc6d9e56dc13165b3db9ce839d20f6fb
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-03-18 18:01:56 +0500

    Added admin register for GroupArticles model.

--- apps/main/admin.py
+++ apps/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, GroupArticles
 from simple_history.admin import SimpleHistoryAdmin
 
 
@@ -8,5 +8,15 @@ class ArticleHistoryAdmin(SimpleHistoryAdmin):
     history_list_display = ["url"]
     list_display_links = ('title',)
     search_fields = ['title', 'author__username']
+
+
+@admin.register(GroupArticles)
+class GroupArticlesAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'author')
+	list_display_links = ('name',)
+	search_fields = ('name',)
+	readonly_fields = ('slug', 'created_date')
+	save_as = True
+
 
 admin.site.register(Article, ArticleHistoryAdmin)
