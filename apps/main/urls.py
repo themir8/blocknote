30fd72e0131b998c5d60ab08f4a92ba03ff4308b
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-03-18 19:55:56 +0500

    Added new paths for GroupList, PublicGroupList and GroupDetail.

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path, include
-from main.views import ArticleCreate, ArticleDetail
-from main.views import RegisterPage, CustomLoginView, ArticleList, ArticleViewEdit
+from main.views import (
+	RegisterPage, 
+	CustomLoginView, 
+	ArticleList, 
+	ArticleViewEdit,
+	ArticleCreate,
+    PublicGroupList,
+	GroupList,
+    GroupDetail
+	)
 from django.contrib.auth.views import LogoutView
 
 
@@ -11,5 +19,9 @@ urlpatterns = [
 
     path('', ArticleCreate.as_view(), name="index"),
     path('all/', ArticleList.as_view(), name="postlist"),
-    path('<slug:slug>', ArticleViewEdit, name="article"),
+    path('post/<slug:slug>', ArticleViewEdit, name="article"),
+
+    path('group/', GroupList.as_view(), name="privategrouplist"),
+    path('groups/', PublicGroupList.as_view(), name="publicgrouplist"),
+    path('groups/<slug:slug>/', GroupDetail.as_view(), name="groupdetail"),
 ]
