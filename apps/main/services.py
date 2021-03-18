cab6f62c0866cfea1804ecfae47d2bad807ed362
mirsaid-mirzohidov <mirzohidovm8@gmail.com>
2021-03-18 18:10:52 +0500

    Changed generate_url function.

--- apps/main/services.py
+++ apps/main/services.py
@@ -2,4 +2,5 @@ import time
 from django.utils.text import slugify
 
 def generate_url(request):
-	slugify(request.POST.get("title") + "-" + str(time.strftime("%m-%d")))
+	"""Generate urls"""
+	return slugify(request.POST.get("title") + "-" + str(time.strftime("%m-%d")))
